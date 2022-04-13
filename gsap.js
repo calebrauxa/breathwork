5e13fc074e51e429270acb1bcc849d06d20bbea4
calebrauxa <cmoore@rauxa.com>
2022-04-12 17:01:11 -0700

    more

--- gsap.js
+++ gsap.js
@@ -3,24 +3,26 @@ let tl = gsap.timeline({
 });
 let holdTime = 3.3;
 let holdItDiv = document.querySelector('#holdit');
+let squirc3path = document.querySelector('#squirc3path');
 
 // rotate (elena if you are looking at this.. if you use gsap.to, its outside of the 'timeline', so here the rotation will just happen forever since I have repeat set to -1 (infinity))
 gsap.to("svg", {
-    duration: 10, 
+    duration: 12, 
     rotation: 360, 
     repeat: -1, 
     ease: Linear.easeNone
 });
 
-// then you can use the timeline (tl.to) to do things in sequence with one another
-// grow in size + opacity to full
 // 1 - BREATH IN
 tl.to("svg", {
     width: "40%", 
     opacity: 1, 
     duration: holdTime, 
-    ease: Linear.easeNone,
-    onStart: () => {console.log('1 - BREATH IN started')}
+    ease: "power1.out",
+    onStart: () => {
+        console.log('1 - BREATH IN started')
+        holdItDiv.innerHTML = 'breathe in'
+    }
 });
 
 // 2 - HOLD
@@ -28,39 +30,35 @@ tl.to("svg", {
     duration: holdTime,
     onStart: () => {
         console.log('2 - HOLD started')
+        holdItDiv.innerHTML = 'hold it'
+        squirc3path.classList.add("flash")
+    },
+    onComplete: () => {
+        squirc3path.classList.remove("flash")
     }
 });
 
-    tl.to(holdItDiv, {
-        opacity: 1,
-        duration: .1,
-    }, "<")
-    tl.to(holdItDiv, {
-        opacity: 0,
-        duration: .1,
-    },"+=.5")
-
-// shrink back down + lower opacity
 // 3 - BREATH OUT
 tl.to("svg", {
     width: "20%", 
     opacity: .3, 
     duration: holdTime, 
-    ease: Linear.easeNone,
-    onStart: () => {console.log('3 - BREATH OUT started')}
+    ease: "power1.out",
+    onStart: () => {
+        console.log('3 - BREATH OUT started')
+        holdItDiv.innerHTML = 'breathe out'
+    }
 });
 
 // 4 - HOLD
 tl.to("svg", {
     duration: holdTime,
-    onStart: () => {console.log('4 - HOLD started')}
+    onStart: () => {
+        console.log('4 - HOLD started')
+        holdItDiv.innerHTML = 'hold it'
+        squirc3path.classList.add("flash")
+    },
+    onComplete: () => {
+        squirc3path.classList.remove("flash")
+    }
 });
-
-    tl.to(holdItDiv, {
-        opacity: 1,
-        duration: .1,
-    }, "<")
-    tl.to(holdItDiv, {
-        opacity: 0,
-        duration: .1,
-    },"+=.5")
